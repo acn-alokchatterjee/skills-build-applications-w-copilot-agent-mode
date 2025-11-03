652e9ba785320d6d378f3b7926fbdad2d507a09c
acn-alokchatterjee <alok.k.chatterjee@accenture.com>
2025-11-03 11:49:13 +0000

    "created the database structure"

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(email='test@hero.com', name='Test Hero', team='Marvel', is_superhero=True)
+        self.assertEqual(user.email, 'test@hero.com')
+        self.assertTrue(user.is_superhero)
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='Marvel')
+        self.assertEqual(team.name, 'Marvel')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        activity = Activity.objects.create(user='Test Hero', type='Running', duration=30, date='2025-11-03')
+        self.assertEqual(activity.type, 'Running')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        leaderboard = Leaderboard.objects.create(team='Marvel', points=100)
+        self.assertEqual(leaderboard.points, 100)
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do pushups', difficulty='Easy')
+        self.assertEqual(workout.name, 'Pushups')
